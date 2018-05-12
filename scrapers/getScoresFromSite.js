c2c6dca2daf2c115a816fb56d1fb44952b952d10
Josh Alamillo <josh.alamillo@gmail.com>
2018-05-12 14:55:15 -0700

    automated level info fetching

--- scrapers/getScoresFromSite.js
+++ scrapers/getScoresFromSite.js
@@ -60,7 +60,7 @@ const getScoreForHundredSongs = (iteration) => {
   }
 }
 
-getScoreForHundredSongs(0);
+// getScoreForHundredSongs(0);
 
 // for (let id = 700; id < 784; id++) {
 //   axios.get(`http://skillattack.com/sa4/music.php?index=${id}`)
@@ -96,37 +96,42 @@ getScoreForHundredSongs(0);
 //this adds the correct level info
 
 
-// for (let level = 17; level <= 17; level++) {
-//   axios.get(`http://skillattack.com/sa4/dancer_score.php?_=rival&ddrcode=51457120&style=0&difficulty=${level}`)
-//   .then((response) => {
-//     let splitsArray = response.data.split(';')
-//     let levelAndDifficultyInfo = splitsArray.filter((item) => {
-//       return (item.includes('ddIndex') || item.includes('ddSequence')) && item.includes('new Array')
-//     })
-//     let ids = levelAndDifficultyInfo[0].split('(')[1].split(')')[0].split(',')
-//     let difficulties = levelAndDifficultyInfo[1].split('(')[1].split(')')[0].split(',')
-//     //for each id, update its difficulty to be this level. Tuple contains level(number), id, difficultyCategory
-//     idDifficultyMap = ids.map((id, index) => [level, id, difficulties[index]])
+const fetchLevelInfo = (level) => {
+  axios.get(`http://skillattack.com/sa4/dancer_score.php?_=rival&ddrcode=51457120&style=0&difficulty=${level}`)
+  .then((response) => {
+    let splitsArray = response.data.split(';')
+    let levelAndDifficultyInfo = splitsArray.filter((item) => {
+      return (item.includes('ddIndex') || item.includes('ddSequence')) && item.includes('new Array')
+    })
+    let ids = levelAndDifficultyInfo[0].split('(')[1].split(')')[0].split(',')
+    let difficulties = levelAndDifficultyInfo[1].split('(')[1].split(')')[0].split(',')
+    //for each id, update its difficulty to be this level. Tuple contains level(number), id, difficultyCategory
+    idDifficultyMap = ids.map((id, index) => [level, id, difficulties[index]])
 
-//     idDifficultyMap.forEach((tuple) => {
-//       console.log(tuple)
-//       db.findById(tuple, (data) => {
-//         if (data.length === 0) {
-//           console.log(`could not FIND any song with id ${tuple[1]}`)
-//         }
-//         if (data.length === 1) {
-//           data[0].levels[tuple[2]] = tuple[0]
-//           let newLevels = data[0].levels
-//           db.updateLevel(newLevels, tuple[1])
-//         }
+    idDifficultyMap.forEach((tuple, idx, map) => {
+      console.log(tuple)
+      db.findById(tuple, (data) => {
+        if (data.length === 0) {
+          console.log(`could not FIND any song with id ${tuple[1]}`)
+        }
+        if (data.length === 1) {
+          data[0].levels[tuple[2]] = tuple[0]
+          let newLevels = data[0].levels
+          db.updateLevel(newLevels, tuple[1])
+        }
+        if (idx === map.length - 1 && level < 19) {
+          fetchLevelInfo(level + 1)
+        }
         
-//       })
-//     })
-//   })
-//   .catch((err) => {
-//     console.log("Error! ", err)
-//   })
-// }
+      })
+    })
+  })
+  .catch((err) => {
+    console.log("Error! ", err)
+  })
+}
+
+fetchLevelInfo(1)
 
 
 
