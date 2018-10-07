b1eb4cfb340f207eedf3c9a2f49ac6fe545cd7c1
Josh Alamillo <josh.alamillo@gmail.com>
2018-10-06 18:01:38 -0700

    Done with part one of refactoring

--- database/seed.js
+++ database/seed.js
@@ -1,8 +1,6 @@
 const songInfo = require('../scrapers/songInfo.json')
 const db = require('./index')
 
-console.time()
-
 db.clearAllSongs(function() {
   console.log('cleared all songs')
   let infoByLevel = {};
@@ -23,24 +21,6 @@ db.clearAllSongs(function() {
     }   
   })
 })
-
-
-
-
-
-  // for (let i = 0; i < Object.keys(songInfo).length; i++) {
-  //   db.saveSong(songInfo[i])
-  //   songInfo[i].levels.forEach((level, index) => {
-  //     let songInfoTuple = [songInfo[i].title, index];
-  //     infoByLevel[level] = infoByLevel[level] ? infoByLevel[level].concat(songInfoTuple) : [songInfoTuple];
-  //   })
-  //   if (i === Object.keys(songInfo).length - 1) {
-  //     Object.keys(infoByLevel).forEach((level) => {
-  //       db.saveLevel(Object.keys(songInfo)[i], infoByLevel[level])
-  //     })
-  //   }
-  // }
-//})
 
 
 setTimeout(() => {
